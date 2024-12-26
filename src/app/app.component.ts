9967744dfc392ab689c4568152e75d0109d7fd61
vijaykumar5691 <vikashvijay81@gmail.com>
2024-12-26 15:14:00 +0530

    headr and footer added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/layouts/header/header.component';
+import { FooterComponent } from './shared/layouts/footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ecommerce';
